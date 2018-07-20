ee43527769f62017cf6598c3f8092707be282e7f
Alex Scott <williamascott@cedarville.edu>
2018-07-20 07:24:59 -0400

    adding in some hooks for load testing

--- service-one/app.js
+++ service-one/app.js
@@ -2,7 +2,8 @@ require('dotenv').config();
 const kit = require('microservice-kit');
 
 try {
-    kit.startServer(() => console.log('Hello, world!'), process.env.SPACE_ID, process.env.CMA_ACCESS_TOKEN);
+    kit.startServer(() => console.log('Hello, world!'), 
+        process.env.SPACE_ID, process.env.CMA_ACCESS_TOKEN, 'message');
 }
 catch (e) {
     console.log(e);
